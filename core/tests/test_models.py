33c32c036aa890a1523b13aea756da5af44af9fd
crashlaker <crashlaker@gmail.com>
2020-11-18 19:18:06 -0500

    finished

---
+++ core/tests/test_models.py
@@ -0,0 +1,95 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+from unittest.mock import patch
+
+
+def sample_user(email='test@test.com', password='testpass'):
+    return get_user_model().objects.create_user(
+            email=email, 
+            password=password
+    )
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@test.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@LONDONAPPDEV.COM'
+        user = get_user_model().objects.create_user(
+            email,
+            'test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                None,
+                'test123'
+            )
+    
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@test.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+
+    def test_tag_str(self):
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+       uuid = 'test-uuid' 
+       mock_uuid.return_value = uuid
+
+       file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+       exp_path = f'uploads/recipe/{uuid}.jpg'
+       self.assertEqual(file_path, exp_path)
+
+
+
+
+
+
